f41f9ed3018cae5d4854a6fc0960426ae35bdc08
Jack <jack_120@hotmail.co.uk>
2018-05-03 13:49:54 +0100

    writing mandelbrot to image file

--- ch2/mandelbrot/src/main.rs
+++ ch2/mandelbrot/src/main.rs
@@ -1,6 +1,10 @@
 extern crate num;
+extern crate image;
 use num::Complex;
 use std::str::FromStr;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
 
 fn main() {
 
@@ -70,6 +74,38 @@ fn pixel_to_point(bounds: (usize, usize), pixel: (usize, usize), upper_left: Com
         // Why subtraction here ? pixel.1 increases as we go down,
         // but the imaginary component increases as we go up.
     }
+}
+
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+///
+/// The 'bounds' argument gives the width and height of the buffer 'pixels',
+/// which holds one grayscale pixel per byte. The 'upper_left' and 'lower_right'
+/// arguments specify points on the complex plane corresponding to the upper-
+/// left and lower-right corners of the pixel buffer.
+fn render(pixels: &mut[u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>){
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0 .. bounds.1 {
+        for column in 0 .. bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] =
+                match escape_time(point,255){
+                    None => 0,
+                    Some(count) => 255 - count as u8
+                };
+        }
+    }
+}
+
+/// Write the buffer 'pixels', whose dimensions are given by 'bounds', to the
+/// file named 'filename'.
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+
+    Ok(())
 }
 
 #[test]
