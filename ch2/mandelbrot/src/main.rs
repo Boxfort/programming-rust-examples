c26a8cc4b31f4b107de003274a3c3c0804232ed5
Jack <jack_120@hotmail.co.uk>
2018-05-03 11:53:42 +0100

    Added parse_pair function

--- ch2/mandelbrot/src/main.rs
+++ ch2/mandelbrot/src/main.rs
@@ -1,8 +1,29 @@
 extern crate num;
 use num::Complex;
+use std::str::FromStr;
 
 fn main() {
 
+}
+
+/// Parse the string 's' as a coordinate pair, like '"400x600"' or '"1.0,0.5"'.
+///
+/// Specifically, 's' should have the form <left><sep><right>, where <sep> is
+/// the character given by the 'seperator' argument, and <left> and <right> are both
+/// strings that can be parsed by 'T::from_str'.
+///
+/// If 's' has the proper form, return 'Some<(x,y)>'. If it doesn't parse
+/// correctly, return 'None'.
+fn parse_pair<T: FromStr>(s: &str, seperator: char) -> Option<(T, T)> {
+    match s.find(seperator){
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])){
+                (Ok(l), Ok(r)) => Some((l,r)),
+                _ => None
+            }
+        }
+    }
 }
 
 /// Try to determine if 'c' is in the Mandelbrot set, using at most 'limit'
