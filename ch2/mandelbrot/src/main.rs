2c5e7d316bbb3fdb54bd5b3d05d348c616393c20
Jack <jack_120@hotmail.co.uk>
2018-05-03 14:10:20 +0100

    sequential mandelbrot set

--- ch2/mandelbrot/src/main.rs
+++ ch2/mandelbrot/src/main.rs
@@ -5,9 +5,26 @@ use std::str::FromStr;
 use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
+use std::io::Write;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
 
+    if args.len() != 5 {
+        writeln!(std::io::stderr(), "Useage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT").unwrap();
+        writeln!(std::io::stderr(), "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0]).unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG to file");
 }
 
 /// Parse the string 's' as a coordinate pair, like '"400x600"' or '"1.0,0.5"'.
